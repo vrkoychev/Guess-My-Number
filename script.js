548f298b904190e0e5539428fa4e4ded002b4efc
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-10-28 11:08:14 +0300

    Final Product

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = document.querySelector('.guess').value;
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // When player wins
+  } else if (guess == secretNumber) {
+    displayMessage('✔️ Correct Number!');
+    displayNumber(secretNumber);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When player is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      displayScore(0);
+
+      document.querySelector('body').style.backgroundColor = '#A52A2A';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  displayNumber('?');
+  displayScore(score);
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
